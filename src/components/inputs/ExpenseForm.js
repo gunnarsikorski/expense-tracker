da767ae23e29683dbcce075cb91e2f4e85006c6e
gunnarsikorski <gunnarsikorski@gmail.com>
2021-05-17 17:59:07 -0500

    Adding state, listening to user input

--- src/components/inputs/ExpenseForm.js
+++ src/components/inputs/ExpenseForm.js
@@ -1,26 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
+	const [title, setTitle] = useState('');
+	const [amount, setAmount] = useState('');
+	const [date, setDate] = useState('');
+
+	const handleTitleChange = (event) => {
+		setTitle(event.target.value);
+	};
+	const handleAmountChange = (event) => {
+		setAmount(event.target.value);
+	};
+	const handleDateChange = (event) => {
+		setDate(event.target.value);
+	};
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		const expenseData = {
+			title: title,
+			amount: amount,
+			date: new Date(date),
+		};
+		setTitle('');
+		setAmount('');
+		setDate('');
+	};
+
 	return (
-		<form>
+		<form onSubmit={handleSubmit}>
 			<div className='new-expense__controls'>
 				<div className='new-expense__control'>
 					<label>Expense Name</label>
-					<input type='text' />
+					<input type='text' value={title} onChange={handleTitleChange} />
 				</div>
 				<div className='new-expense__control'>
 					<label>Amount</label>
-					<input type='number' min='.01' step='.01' />
+					<input
+						type='number'
+						min='.01'
+						step='.01'
+						value={amount}
+						onChange={handleAmountChange}
+					/>
 				</div>
 				<div className='new-expense__control'>
 					<label>Date</label>
-					<input type='date' min='2019-01-01' max='2024-12-31'/>
+					<input
+						type='date'
+						min='2019-01-01'
+						max='2024-12-31'
+						value={date}
+						onChange={handleDateChange}
+					/>
 				</div>
 			</div>
-            <div className='new-expense__actions'>
-                <button type='submit'>Add expense</button>
-            </div>
+			<div className='new-expense__actions'>
+				<button type='submit'>Add expense</button>
+			</div>
 		</form>
 	);
 };

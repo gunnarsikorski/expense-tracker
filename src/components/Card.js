dc07b0f32afe26ba877ba4afd20337ee473d28a4
gunnarsikorski <gunnarsikorski@gmail.com>
2021-05-17 12:19:40 -0500

    Created wrapper Card component

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Card.css'
+
+function Card(props) {
+    const classes = 'card ' + props.className
+    // line above allows this card to take classNames into account from other css classNames
+    // props.children allows use of card as a wrapper
+    return (
+        <div className={classes}>
+            {props.children}
+        </div>
+    );
+}
+
+export default Card;
